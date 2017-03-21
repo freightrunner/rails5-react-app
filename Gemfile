35be17cd0cba1fb0701b1d470948ef197572e4fc
Brian Arpaio <arpaio@gmail.com>
2017-03-21 03:41:35 +0000

    created a form for new lifts, and now will auto-calculate value of 1 RM as data is filled out

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 
-gem 'bootstrap', '~> 4.0.0.alpha3.1'
+gem 'bootstrap', '~> 4.0.0.alpha6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 # Use sqlite3 as the database for Active Record
--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,5 +1,22 @@
 class LiftsController < ApplicationController
-    def index
-        @lifts = Lift.all
+  def create
+    @lift = Lift.new(lift_params)
+    
+    if @lift.save
+        render json: @lift
+    else
+        render json: @lift.errors, status: :unprocessable_entity
     end
+  end
+  
+  def index
+    @lifts = Lift.all
+  end
+  
+  private
+  def lift_params
+    params.require(:lift).permit(:date, :liftname, :ismetric, :weightlifted, :repsperformed, :onerm)
+  end
+  
 end
+
